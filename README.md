506adfa5bb2cdd1d58c62efc1fe6e458adf3c1a1
Tap <116336366+varTapexe@users.noreply.github.com>
2023-06-27 11:37:15 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,47 @@
+
+
+# Wizard101 Chaos Mode Challenge - By [Tap](https://www.tapwater.dev): 
+*This file will tell you everything you need to know about this script!*
+
+# What do you need to change?
+### Your Wizard101 path. In the `main.py` file you will see this line:
+```python
+    utils.override_wiz_install_location(r'E:\Kingsisle Entertainment\Wizard101')
+```
+Replace E:\Kingsisle Entertainment\Wizard101 with YOUR Wizard101 path.
+
+> Right click your **Wizard101** icon.
+> Click **properties**.
+> Copy the text in the `Start in:` box __**without** the quotation marks.__
+
+# How do you run this?
+
+1. Install the [latest version of python](https://www.python.org/downloads/).
+1. Install the [latest version of GIT](https://git-scm.com/download/win).
+2. Install [Visual Studio Code](https://code.visualstudio.com)
+3. Open Visual Studio Code
+4. Click **Terminal** > **New Terminal**
+5. In the terminal type or paste: `python -m pip install -r requirements.txt`
+6. After everything is installed type `py main.py` to run the file.
+
+# Speedrun Rules.
+
+### Rules:
+1. Must start on a **new** wizard. Any school is allowed but make sure you are on a brand new wizard/account.
+2. Timer starts once you first talk to Merle Ambrose in his office. (When you get teleported to Unicorn Way)
+3. No pets besides the one you get in the main quest.
+4. No mounts, this includes the 1 day broom or any other mounts recieved from quests.
+5. No crown shop items or henchmen are allowed.
+5. Must be solo, make sure you're in a pretty empty realm to guarentee no one enters your fights.
+6. Bazaar is allowed, if you have some strategy with items you can buy from the bazaar to make your time quicker that is allowed.
+7. In recording you must have your game, stopwatch, and Visual Studio Code window in view the entire time.
+8. Use common sense, some rules may not be written down here but just use your brain on what would be considered cheating.
+
+### Instructions:
+
+For this speedrun you will not be completing Wizard City, I have changed the goal to **Defeat the Kraken in Triton Avenue** to allow people without membership to participate in this challenge.
+
+# How do I get support on this?
+
+I probably won't respond right away but you can join my [Discord server](https://discord.com/invite/49EZMGKx6p) and message me.
+- I will probably update this code every now and then so feel free to ⭐ **Star** the GitHub repo.
---
+++ main.py
@@ -0,0 +1,243 @@
+import asyncio
+from pyfiglet import Figlet
+from termcolor import colored, cprint
+import math
+from colorama import init, Fore, Back, Style
+from wizwalker import ClientHandler
+from wizwalker.memory.memory_objects.camera_controller import ElasticCameraController, CameraController
+from wizwalker.errors import HookNotActive
+from wizwalker import Client, utils, Orient, XYZ
+import random
+
+# Customize Below:
+utils.override_wiz_install_location(r'E:\Kingsisle Entertainment\Wizard101') # Enter your wizard101 path.
+
+init() #for logging
+
+# DO NOT TOUCH ANYTHING IN THE DASHES -----------------------------------------------------
+
+def calc_Distance(xyz_1 : XYZ, xyz_2 : XYZ):
+    # calculates the distance between 2 XYZs
+    return math.sqrt((pow(xyz_1.x - xyz_2.x, 2.0)) + (pow(xyz_1.y - xyz_2.y, 2.0)) + (pow(xyz_1.z - xyz_2.z, 2.0)))
+
+async def is_free(client: Client):
+	# Returns True if not in combat, loading screen, or in dialogue.
+	return not any([await client.is_loading(), await client.in_battle()])
+
+async def is_in_combat(client: Client):
+	# Returns True if not in combat, loading screen, or in dialogue.
+	return not any([await client.in_battle()])
+
+async def wait_for_free(client: Client, wait_for_not: bool = False, interval: float = 0.25):
+    if wait_for_not:
+        while await is_free(client):
+            await asyncio.sleep(interval)
+
+    else:
+        while not await is_free(client):
+            await asyncio.sleep(interval)
+
+async def wait_for_combat(client: Client, wait_for_not: bool = False, interval: float = 0.25):
+    if wait_for_not:
+        while await is_in_combat(client):
+            await asyncio.sleep(interval)
+
+    else:
+        while not await is_in_combat(client):
+            await asyncio.sleep(interval)
+
+async def set_player_scale(scale_to_set, client: Client, default_scale: float = 1.0):
+    scale = await client.body.scale()
+    if scale:
+        await client.body.write_scale(scale_to_set)
+    else:
+        await client.body.write_scale(default_scale)
+        await set_player_scale(scale_to_set, client)
+        await asyncio.sleep(1)
+    # await client.send_key(Keycode.W, 0.1)
+
+async def set_player_speed(speed_to_set, client: Client, default_speed: int = 1):
+    speed = await client.client_object.speed_multiplier()
+    if speed:
+        # print("Set speed!")
+        await client.client_object.write_speed_multiplier(speed_to_set)
+    else:
+        # print("Speed wasn't found for some reason? Trying again...")
+        await client.client_object.write_speed_multiplier(default_speed)
+        await set_player_speed(speed_to_set, client)
+        await asyncio.sleep(1)
+
+async def set_camera_distance(min, max, set, zoom, client: Client, default_distance: float = 300.0):
+    camera: ElasticCameraController = await client.game_client.elastic_camera_controller()
+    client_object = await client.body.parent_client_object()
+    await camera.write_attached_client_object(client_object)
+    if set and min and max:
+        await camera.write_distance_target(set)
+        await camera.write_distance(set)
+        await camera.write_min_distance(min)
+        await camera.write_max_distance(max)
+        if zoom:
+            await camera.write_zoom_resolution(zoom)
+    else:
+        await camera.write_distance_target(default_distance)
+        await camera.write_distance(default_distance)
+        await camera.write_min_distance(150.0)
+        await camera.write_max_distance(450.0)
+
+async def update_camera(pitch, roll, yaw, camera: CameraController):
+    camera_rot = await camera.orientation()
+    if camera_rot:
+        if pitch == None:
+            pitch = camera_rot.pitch
+        if roll == None:
+            roll = camera_rot.roll
+        if yaw == None:
+            yaw = camera_rot.yaw
+        await camera.update_orientation(Orient(pitch, roll, yaw))
+
+async def update_player(pitch, roll, yaw, client: Client):
+    player_rot = await client.body.orientation()
+    if player_rot:
+        if pitch == None:
+            pitch = player_rot.pitch
+        if roll == None:
+            roll = player_rot.roll
+        if yaw == None:
+            yaw = player_rot.yaw
+        await client.body.write_orientation(Orient(pitch, roll, yaw))
+
+async def load_check(client: Client):
+    while True:
+        player_size = await client.body.scale()
+        player_speed = await client.client_object.speed_multiplier()
+        if await client.is_loading() == True:
+            print(Fore.CYAN + "Detected zone change, reapplying size/speed..." + Fore.RESET)
+            while await client.is_loading() == True:
+                await asyncio.sleep(0.1)
+            await asyncio.sleep(0.25) # increase if you load slower
+            await set_player_scale(player_size, client)
+            await set_player_speed(player_speed, client)
+        await asyncio.sleep(1)
+# This function checks if you encounter a loading screen (or enter a new zone, which clears all effects)
+
+async def unhook_ww(client: Client, client_camera: CameraController, handler: ClientHandler):
+    print(Back.RED + Fore.WHITE + "Unhooking..." + Back.RESET)
+    await set_player_scale(1.0, client)
+    await set_player_speed(0, client)
+    await set_camera_distance(150, 450, 300, 150, client)
+    await update_camera(None, 0.0, None, client_camera)
+    await update_player(0.0, None, None, client)
+    await handler.close()
+    exit()
+# DO NOT TOUCH ANYTHING IN THE DASHES -----------------------------------------------------
+
+client = False
+async def start():
+    global client
+    
+    f = Figlet(font="ogre")
+    TITLE_SCREEN = f"""
+    { f.renderText("Tap Challenge")[:-3] }
+    { colored(" Created with ♥ by Tap! ", "blue") }
+
+    Goodluck! :)
+    """
+    print(TITLE_SCREEN)
+    print(Fore.BLUE + "LAUNCHING | " +
+    Fore.GREEN + "Starting in 3 seconds...") # Feel free to change the 3 to any amount of delay before it starts. 
+    await asyncio.sleep(3) # Change this too if you do
+    try:
+        handler = ClientHandler()
+        client = handler.get_new_clients()[0]
+        client_camera = await client.game_client.selected_camera_controller()
+        try:
+            print(Fore.BLUE + "HOOKING | " +
+    Fore.WHITE + "Hooking clients...")
+            print(Style.DIM + f"(If nothing happens after a few seconds make sure you're IN THE GAME, not in the wizard selection.)" + Style.RESET_ALL)
+            await client.activate_hooks()
+            print(Fore.BLUE + "HOOKING | " +
+    Fore.GREEN + "Hooked!")
+
+            try:
+                print(Fore.YELLOW + f'Starting scripts: (Press CTRL+C in the terminal to stop.)\n')
+                tasks = [boost(client, client_camera, handler), grow(client, client_camera, handler), npc(client, client_camera, handler), load_check(client)]
+                await asyncio.gather(*tasks)
+            except:
+                try:
+                    await unhook_ww(client, client_camera, handler)
+                except HookNotActive:
+                    print("")
+                exit()
+        except Exception as e:
+            await unhook_ww(client, client_camera, handler)
+            print(Fore.RED + "ERR | " +
+    Fore.RED + "An error occured while hooking clients: " + Fore.WHITE + e)
+
+    except:
+        print(Fore.RED + "An error occured, try opening or restarting Wizard101." + Fore.RESET)
+        try:
+            await unhook_ww(client, client_camera, handler)
+        except:
+            print("")
+        exit()
+
+async def boost(client: Client, camera: CameraController, handler: ClientHandler):
+    if client:
+        print(Fore.CYAN + "Initializing | " +
+    Fore.GREEN + f"Random speed every 10 seconds.")
+        print(Fore.YELLOW + "Initialized | " +
+    Fore.GREEN + f"Random speed loop started." + Fore.WHITE)
+        while True: # Begin loop
+            await asyncio.sleep(10) # wait 10 seconds
+            random_number = 0
+            sign = random.choice([-1, 1])
+            if sign == -1:
+                random_number = random.randint(-80, 0)
+            else:
+                random_number = random.randint(0, 10000)
+            print(Fore.BLUE + "New Speed: " +
+    Fore.LIGHTGREEN_EX + f"{random_number}%" + Fore.WHITE)
+            await set_player_speed(random_number, client)
+    else: 
+        print(Fore.RED + "Client was never initiated." + Fore.RESET)
+
+async def grow(client: Client, camera: CameraController, handler: ClientHandler):
+    if client:
+        print(Fore.CYAN + "Initializing | " +
+        Fore.GREEN + f"Grow player size 0.1% every second.")
+        player_size = 1
+        print(Fore.YELLOW + "Initialized | " +
+    Fore.GREEN + f"Player now growing." + Fore.WHITE)
+        while True: # Begin loop
+            player_size += 0.001
+            await set_player_scale(player_size, client)
+            await asyncio.sleep(1)
+    else: 
+        print(Fore.RED + "Client was never initiated." + Fore.RESET)
+
+async def npc(client: Client, camera: CameraController, handler: ClientHandler):
+    if client:
+        print(Fore.CYAN + "Initializing | " +
+        Fore.GREEN + f"After every fight, tp to a random mob/npc.")
+        entity_list = []
+        print(Fore.YELLOW + "Initialized | " +
+    Fore.GREEN + f"Combat detector enabled.\n" + Fore.WHITE)
+        while True: # Begin loop
+            await wait_for_combat(client, True) # Waiting for wizard to join a fight.
+            await wait_for_combat(client) # Once in a fight, wait for fight to finish.
+
+            # Then tp to random entity:
+            ent_list = await client.get_base_entity_list()
+            for entity in ent_list:
+                if await entity.display_name():
+                    if calc_Distance(await client.body.position(), await entity.location()) < 25000:
+                        entity_list.append(entity)
+            chosen_entity = random.choice(entity_list)
+            await client.teleport(await chosen_entity.location())
+            print(Fore.CYAN + f"FIGHT COMPLETED |" + Fore.LIGHTGREEN_EX +  f"Teleported to" + Fore.BLUE + f" {await chosen_entity.display_name()}." + Fore.RESET)
+            entity_list = [] # Reset entity list.
+    else: 
+        print(Fore.RED + "Client was never initiated." + Fore.RESET)
+    
+if __name__ == "__main__":
+    asyncio.run(start())
